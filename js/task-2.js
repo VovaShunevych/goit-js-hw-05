8a48597ab5bdf8bb1e257066a3d7a848ed3db8ed
VovaShunevych <vovashynu@gmail.com>
2024-02-11 21:30:36 +0200

    змінено  21:30

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,6 @@
 
-const getUsersWithFriend = (users, friendName) => users.filter((user) => user.friends === friends);
-
-
+const getUsersWithFriend = (users, friendName) =>
+{ return users.filter(({ friends }) => friends.includes(friendName)) }
 
 const allUsers = [
   {
@@ -33,6 +32,30 @@ const allUsers = [
     friends: ["Goldie Gentry", "Briana Decker"]
   }
 ];
+console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+// [
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
 
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+// [
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
 
 
